b9a4ee572d93ed497df4a6494ba5e02e942cd025
student <joy.fan00@gmail.com>
2023-04-24 09:53:37 -0400

    edits on input_concert_info UI

--- src/UI.java
+++ src/UI.java
@@ -7,7 +7,6 @@ public class UI {
     public void goToHome () {
         // Setting Home Screen
         JFrame home = new JFrame("Home Screen");
-        home.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         home.setSize(1000,1000);
         // Buttons to indicate which action the user would like to do on the database
         JButton songinfo = new JButton("Song Info Based on Concert Info");
@@ -56,8 +55,37 @@ public class UI {
     // inputing concert info to filter a specific conert
     public void inputConcertInfo () {
         JFrame concertinfo = new JFrame("Concert Info");
+        JPanel concert_artist_info = new JPanel();
+        JPanel buttons_field = new JPanel();
         JLabel artist = new JLabel("Artist Name: ");
         JTextField artist_input = new JTextField();
+        JLabel time = new JLabel("Time of Concert:");
+        JTextField time_input = new JTextField();
+        JLabel venue = new JLabel("City of Concert:");
+        JTextField venue_input = new JTextField();
+        JButton back_to_home = new JButton("Back to Home");
+        back_to_home.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                goToHome();
+            }
+        });
+        JButton submit = new JButton("Submit Concert Info");
+
+        concert_artist_info.setLayout(new BoxLayout(concert_artist_info, BoxLayout.PAGE_AXIS));
+        buttons_field.setLayout(new FlowLayout());
+
+        concert_artist_info.add(artist);
+        concert_artist_info.add(artist_input);
+        concert_artist_info.add(time);
+        concert_artist_info.add(time_input);
+        concert_artist_info.add(venue);
+        concert_artist_info.add(venue_input);
+        buttons_field.add(back_to_home);
+        buttons_field.add(submit);
+        concertinfo.add(concert_artist_info, BorderLayout.PAGE_START);
+        concertinfo.add(buttons_field, BorderLayout.PAGE_END);
+
         concertinfo.add(artist);
         concertinfo.add(artist_input);
         concertinfo.setSize(500, 500);
